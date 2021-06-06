8366311326220cb05a9492be09e36ac600719676
AnantDuhan <duhananant@gmail.com>
2021-06-06 17:00:41 +0530

    Information to COVID

---
+++ src/InfoBox.js
@@ -0,0 +1,32 @@
+/***
+ * @Author:xxx
+ * @Date:2021-06-06 15:54:52
+ * @LastModifiedBy:xxx
+ * @Last Modified time:2021-06-06 15:54:52
+ */
+
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React from "react";
+
+function InfoBox({ title, cases, total, props }) {
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        {/* Title i.e. Coronavirus Cases */}
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
+
+        {/* +120k Number of cases */}
+        <h2 className="infoBox__cases">{cases}</h2>
+
+        {/* 1.2M Total */}
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
