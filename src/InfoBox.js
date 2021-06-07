ec9ab3291439cc9fd4fdcc520ebda32ca2dc2337
AnantDuhan <duhananant@gmail.com>
2021-06-07 10:58:03 +0530

    minor bugs fixed

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,26 +1,30 @@
-/***
- * @Author:xxx
- * @Date:2021-06-06 15:54:52
- * @LastModifiedBy:xxx
- * @Last Modified time:2021-06-06 15:54:52
- */
-
-import { Card, CardContent, Typography } from "@material-ui/core";
 import React from "react";
-
-function InfoBox({ title, cases, total, props }) {
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
+function InfoBox({ title, isRed, isGrey, active, cases, total, ...props }) {
   return (
-    <Card className="infoBox">
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      } ${isGrey && "infoBox--grey"}`}
+    >
       <CardContent>
-        {/* Title i.e. Coronavirus Cases */}
+        {/* Title */}
         <Typography className="infoBox__title" color="textSecondary">
           {title}
         </Typography>
 
-        {/* +120k Number of cases */}
-        <h2 className="infoBox__cases">{cases}</h2>
+        {/* Number of Cases */}
+        <h2
+          className={`infoBox__cases ${!isRed && "infoBox__cases--green"} ${
+            isGrey && "infoBox__cases--grey"
+          }`}
+        >
+          {props.isloading ? <i className="fa fa-cog fa-spin fa-fw" /> : cases}
+        </h2>
 
-        {/* 1.2M Total */}
+        {/* Total Cases */}
         <Typography className="infoBox__total" color="textSecondary">
           {total} Total
         </Typography>
