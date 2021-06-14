7f465f09db2ab38993290f43683f58ab99250f9a
AnantDuhan <duhananant@gmail.com>
2021-06-15 00:04:54 +0530

    firebaseConfig

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYgZXKMmc6qr_fWfM5YrnV8jFr1ZrB4Q8",
+    authDomain: "covid-19-tracker-af438.firebaseapp.com",
+    projectId: "covid-19-tracker-af438",
+    storageBucket: "covid-19-tracker-af438.appspot.com",
+    messagingSenderId: "801645202637",
+    appId: "1:801645202637:web:873d67e128fdd15d276d84",
+    measurementId: "G-6DBDM11SST"
+  };
