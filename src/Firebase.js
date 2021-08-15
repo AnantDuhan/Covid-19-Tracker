d50b827b45e6664a7428b00dc2f0bee3e0900bfd
AnantDuhan <duhananant@gmail.com>
2021-08-15 22:04:24 +0530

    firebase deploy

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,9 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAYgZXKMmc6qr_fWfM5YrnV8jFr1ZrB4Q8",
-    authDomain: "covid-19-tracker-af438.firebaseapp.com",
-    projectId: "covid-19-tracker-af438",
-    storageBucket: "covid-19-tracker-af438.appspot.com",
-    messagingSenderId: "801645202637",
-    appId: "1:801645202637:web:873d67e128fdd15d276d84",
-    measurementId: "G-6DBDM11SST"
-  };
+  apiKey: "AIzaSyAYgZXKMmc6qr_fWfM5YrnV8jFr1ZrB4Q8",
+  authDomain: "covid-19-tracker-af438.firebaseapp.com",
+  projectId: "covid-19-tracker-af438",
+  storageBucket: "covid-19-tracker-af438.appspot.com",
+  messagingSenderId: "801645202637",
+  appId: "1:801645202637:web:42dcf8d5b7f03194276d84",
+  measurementId: "G-LR3EXT5J5E",
+};
